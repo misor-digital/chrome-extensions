0ec7f2b7d72528c2db7ff103c8d49a44af4504ad
misor.digital <misor.digital@gmail.com>
2023-04-15 15:34:18 +0300

    popup.js: generate list of tabs, attached event listener to each item

--- popup.js
+++ popup.js
@@ -9,3 +9,27 @@ const tabs = await chrome.tabs.query({
     sites.extensions
   ],
 });
+
+const collator = new Intl.Collator();
+tabs.sort((a, b) => collator.compare(a.title, b.title));
+
+const template = document.getELementById('li_template');
+const elements = new Set();
+for (const tab of tabs) {
+  const title    = tab.title.split('-')[0].trim();
+  const pathname = new URL(tab.url).pathname.slice('/docs'.length);
+
+  const element = template.content.firstElementChild.cloneNode(true);
+  
+  element.querySelector('.title').textContent    = title;
+  element.querySelector('.pathname').textContent = pathname;
+  element.querySelector('a').addEventListener('click', async () => {
+    // need to focus window as well as the active tab
+    await chrome.tabs.update(tab.id, { active: true });
+    await chrome.windows.update(tab.windowId, { focused: true });
+  });
+
+  elements.add(element);
+}
+
+document.querySelector('ul').append(...elements);
