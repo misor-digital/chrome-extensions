4c39d6b86f63f11ff2c2136ec0cf993dad6e18db
misor.digital <misor.digital@gmail.com>
2023-04-15 12:54:23 +0300

    background.js -> onclick event listener; enum.js -> urls & states

--- background.js
+++ background.js
@@ -1,5 +1,26 @@
+import { chromeDocs, state } from './enums';
+
 chrome.runtime.onInstalled.addListener(() => {
   chrome.action.setBadgeText({
-    text: 'OFF',
+    text: state.OFF,
   });
+});
+
+chrome.action.onClicked.addListener(async (tab) => {
+  if (tab.url.startsWith(chromeDocs.extensions) || tab.url.startsWith(chromeDocs.webstore)) {
+    // Retrieve the action badge to check if the extension is ON or OFF
+    const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
+
+    // Next tate will always be the opposite
+    const nextState =
+      prevState === state.ON ?
+        state.OFF :
+        state.ON;
+
+    // Set the action badge to the next state
+    await chrome.action.setBadgeText({
+      tabId : tabId,
+      text  : nextState
+    });
+  }
 });
---
+++ enums.js
@@ -0,0 +1,9 @@
+export const state = {
+    ON  : 'ON',
+    OFF : 'OFF',
+};
+
+export const chromeDocs = {
+  extensions : 'https://developer.chrome.com/docs/extensions',
+  webstore   : 'https://developer.chrome.com/docs/webstore',
+}
